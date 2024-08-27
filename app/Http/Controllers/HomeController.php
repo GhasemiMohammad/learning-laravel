b58b626d93fc232222213ad8edf2f26db14adf8a
ghasemimohammad <mohammadghasemi824@gmail.com>
2024-08-27 17:01:56 +0330

    has many through relation is complete

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Address;
+use App\Models\Country;
 use App\Models\User;
 use Illuminate\Http\Request;
 
@@ -14,8 +15,10 @@ class HomeController extends Controller
 //        dd($user->address);
 //        $address = Address::find(17);
 //        dd($address->user->name);
-          $user = User::find(38);
-          dd($user->info);
+//          $user = User::find(38);
+//          dd($user->info);
+        $country = Country::find(2);
+        dd($country->posts);
 //        return view('welcome');
     }
 }
---
+++ database/migrations/2024_08_27_130501_rename_contries_to_countries.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::rename('contries', 'countries');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('countries', function (Blueprint $table) {
+            //
+        });
+    }
+};
